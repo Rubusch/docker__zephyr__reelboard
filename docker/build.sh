9a34d5dc8fef8b7c8b4fbe6146394f16a099ba89
Lothar Rubusch <l.rubusch@gmx.ch>
2020-11-30 11:56:12 +0100

    Dockerfile: update, only 'zephyr' folder outside

--- docker/build.sh
+++ docker/build.sh
@@ -19,38 +19,15 @@ if [[ ! -d "${BUILDDIR}/.west" ]]; then
 fi
 
 cd ${BUILDDIR}
-west update
-west zephyr-export
-pip3 install --user -r ${BUILDDIR}/zephyr/scripts/requirements.txt
-
-cd ${BUILDDIR}/zephyr
-git checkout ${ZEPHYR_BRANCH}
+if [[ ! -d ${BUILDDIR}/zephyr ]]; then
+    ## get zephyr sources, if there is nothing
+    echo "get zephyr sources..."
+    cd ${BUILDDIR} && rm -rf zephyr && git clone https://github.com/Rubusch/zephyr.git
+    cd ${BUILDDIR}/zephyr
+    git checkout ${ZEPHYR_BRANCH}
+else
+    echo "zephyr sources found"
+fi
 
 echo "READY."
 echo
-
-
-### TODO review the former setup script
-### prepare SDK environment
-##source ~/env.sh
-#
-##$ cat ./env.sh 
-#export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
-#export ZEPHYR_SDK_INSTALL_DIR=$HOME/zephyr-sdk-0.10.3
-#
-### build for reel board
-#cd ~/zephyrproject/zephyr
-#
-### clean
-#test -d build && rm -rf build
-#
-### build
-#source zephyr-env.sh
-#west build -b reel_board_v2 samples/hello_world
-#
-### obtain build artifacts
-#west flash
-#
-### debugging
-##west debug
-#
